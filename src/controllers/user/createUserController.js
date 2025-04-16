5eb75844f5575bfce3f5bc1c11859adda0092158
Renan Cavichi <renancavichi@gmail.com>
2025-04-16 14:55:52 -0300

    add zod validate user

--- src/controllers/user/createUserController.js
+++ src/controllers/user/createUserController.js
@@ -1,10 +1,19 @@
-import { create } from "../../models/userModel.js"
+import { create, userValidator } from "../../models/userModel.js"
 
 export default async function (req, res){
     const user = req.body
     
     //TODO: Validar os dados do usuário
-    const result = await create(user)
+    const { success, error, data } = userValidator(user, {id: true})
+
+    if(!success){
+        return res.status(400).json({
+            message: "Erro ao validar os dados do usuário!",
+            errors: error.flatten().fieldErrors
+        })
+    }
+
+    const result = await create(data)
 
     if(!result){
         return res.status(500).json({
@@ -14,7 +23,7 @@ export default async function (req, res){
 
     delete result.pass
 
-    return res.json({
+    return res.status(201).json({
         message: "Usuário criado com sucesso!",
         user: result
     })
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,6 +1,44 @@
 import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
 
 const prisma = new PrismaClient()
+
+const userSchema = z.object({
+    id: z.number({
+            invalid_type_error: "O id deve ser um valor numérico.",
+            required_error: "O id é obrigatório."
+        }),
+    name: z.string({
+            invalid_type_error: "O nome deve ser uma string.",
+            required_error: "O nome é obrigatório."
+        })
+        .min(3, {message: "O nome deve ter no mínimo 3 caracteres."})
+        .max(255, {message: "O nome deve ter no máximo 255 caracteres."}),
+    email: z.string({
+            invalid_type_error: "O email deve ser uma string.",
+            required_error: "O email é obrigatório."
+        })
+        .email({message: "Email inválido."}),
+    pass: z.string({
+        invalid_type_error: "A senha deve ser uma string.",
+        required_error: "A senha é obrigatória."
+        })
+        .min(8, {message: "A senha deve ter no mínimo 8 caracteres."})
+        .max(255, {message: "A senha deve ter no máximo 255 caracteres."}),
+    avatar: z.string({
+        invalid_type_error: "O avatar deve ser uma string.",
+        required_error: "O avatar é obrigatório."
+        })
+        .url({message: "Url do avatar inválida."})
+        .optional()
+})
+
+export const userValidator = (user, partial = null) => {
+    if (partial) {
+        return userSchema.partial(partial).safeParse(user)
+    } 
+    return userSchema.safeParse(user)
+} 
 
 export async function create(user){
     const result = await prisma.user.create({
