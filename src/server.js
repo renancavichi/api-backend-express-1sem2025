ec9ff223ba73c39d162128db9ceb581032def226
Renan Cavichi <renancavichi@gmail.com>
2025-04-23 14:35:26 -0300

    organização dos comentários

--- src/server.js
+++ src/server.js
@@ -1,24 +1,19 @@
 // const express = require('express') <- Exemplo de importação com require, mudamos para "import" no package.json type: "module"
 // importacao do pacote express
 import express from 'express'
-
 // importacao dos routers
 import userRouter from './routers/userRouter.js'
 import productRouter from './routers/productRouter.js'
 import { logger } from './middlewares/logger.js'
-// importacao do cors
 import cors from 'cors'
 //import { loggerBody } from './middlewares/loggerBody.js'
 
 // cria o objeto app que tem todas as funções do express
 const app = express()
 
-// middleware para liberar o cors do frontend web
-
-app.use(logger)
-app.use(cors())
-// middleware para o express entender json
-app.use(express.json()) // lê o json e grava o objeto no req.body
+app.use(logger) // Meu middleware de log 
+app.use(cors()) // middleware para liberar o cors do frontend web
+app.use(express.json()) // middleware que lê o json e grava o objeto no req.body
 //app.use(loggerBody)
 
 // criando a rota get no endereço / (raiz: http://localhost:3000/) 
