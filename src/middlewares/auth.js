9ccc5a575ebf4883e2de7cfe28194330cbad1350
Renan Cavichi <renancavichi@gmail.com>
2025-06-18 14:54:27 -0300

    adiciona comentários no código

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,14 +1,17 @@
 import jwt from 'jsonwebtoken'
 
 export const auth = (req, res, next) => {
-    // Authorization: Bearer jwrwe8r2749234
+    // obtem o accessToken que chegou no header Authorization: Bearer jwt_token
     const accessToken = req.headers['authorization']?.split(' ')[1];
 
+    // se não tiver o accessToken, retorna erro
     if (!accessToken) {
         return res.status(401).json({ message: 'Token de autenticação não fornecido.' });
     }
 
+    // verifica se o token é válido
     jwt.verify(accessToken, process.env.JWT_SECRET, (err, decoded) => {
+        // Se o token não for válido ou expirou, retorna erro
         if (err) {
             return res.status(403).json({ message: 'Token inválido ou expirado.' });
         }
